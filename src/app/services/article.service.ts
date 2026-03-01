8e86e450fc8181e9a3258e6b816e51682dfe4eed
Ratsimisetra-Riana <riana.montoya13@gmail.com>
2026-03-02 02:46:45 +0300

    generalize apiurl

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleService {
-  private apiUrl = 'http://localhost:3000/articles';
+  private apiUrl = environment.apiUrl + '/articles';
 
   constructor(private http: HttpClient) {}
 
