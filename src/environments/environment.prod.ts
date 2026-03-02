d512433bcab2ab2ce76fa1711f34e0d9b9f31717
Ratsimisetra-Riana <119068522+Ratsimisetra-Riana@users.noreply.github.com>
2026-03-02 14:29:40 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
 production: true,
-apiUrl: 'https://api.monsite.com' // URL pour la production
+//apiUrl: 'https://api.monsite.com' // URL pour la production
+  apiUrl: 'https://m1p13-riana-backend.vercel.app'
 };
