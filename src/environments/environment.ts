f9a82cef431974da058b43b3f37b737d7f510bdc
Ratsimisetra-Riana <119068522+Ratsimisetra-Riana@users.noreply.github.com>
2026-03-02 14:21:50 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  //apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://m1p13-riana-backend.vercel.app',
   // Supabase configuration
   supabaseUrl: 'https://ueoihbvgqvznalpkzjrp.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlb2loYnZncXZ6bmFscGt6anJwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDcxMjY4MCwiZXhwIjoyMDg2Mjg4NjgwfQ.eFOlu5Yj7EDAOIfPMqF1CeQsZcRTVRXaP24TQNrrP30'
